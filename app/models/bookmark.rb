6b368194f708ca44ac180ee92ab12c28f6185fcd
Karen Gonzalez Reginato <karenreginato@gmail.com>
2024-09-30 12:01:55 +0100

    Added model validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+
+  validates :recipe, presence: true
+  validates :category, presence: true
+  validates :comment, length: { minimum: 6 }
+  validates :recipe_id, uniqueness: { scope: :category_id, message: "already bookmarked in this category" }
 end
